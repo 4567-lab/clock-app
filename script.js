047a66b1ad3c0dfa04f2e795f13a0169812a4b9a
VIJENDRA Patil <vijendrapatil53@gmail.com>
2026-07-11 15:20:39 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -15,20 +15,29 @@ function showPage(id) {
 
 // Digital Clock
 
-function updateClock(){
+function updateClock() {
 
-const now=new Date();
+    const now = new Date();
 
-let h=String(now.getHours()).padStart(2,"0");
-let m=String(now.getMinutes()).padStart(2,"0");
-let s=String(now.getSeconds()).padStart(2,"0");
+    let hours = now.getHours();
+    let minutes = now.getMinutes();
+    let seconds = now.getSeconds();
 
-document.getElementById("clockDisplay").innerHTML=h+":"+m+":"+s;
+    let ampm = hours >= 12 ? "PM" : "AM";
 
+    hours = hours % 12;
+    hours = hours ? hours : 12;
+
+    minutes = String(minutes).padStart(2, "0");
+    seconds = String(seconds).padStart(2, "0");
+
+    document.getElementById("clockDisplay").innerHTML =
+        `${hours}:${minutes}:${seconds} ${ampm}`;
 }
 
-setInterval(updateClock,1000);
+setInterval(updateClock, 1000);
 updateClock();
+
 
 
 // Stopwatch
